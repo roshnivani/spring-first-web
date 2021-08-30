77f04032dfa4b3a4bd45013fdb3d0bd7fc1f630a
Roshani <roshnivani@gmail.com>
2021-08-30 14:40:51 +0530

    done

--- src/main/java/com/dao/CustomerDao.java
+++ src/main/java/com/dao/CustomerDao.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.bean.CustomerBean;
-import com.bean.LoginBean;
 import com.bean.ProductBean;
 
 @Repository
